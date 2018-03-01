ff651eefe79197a0ab61609a2e6ca93ac4ed03b7
Anisha Aggarwal <anishaa@uoregon.edu>
2018-03-01 11:03:30 -0800

    cryto++ lib

---
+++ aes.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: Anisha Aggarwal
+ * File: aes.cpp
+ */
+/*
+ * Generating a random key for each user will occur here
+ * AES encryption and decryption will occur here
+ */
+
+#include <iostream>
+#include <cstring>
+#include <stdlib>
+#include "cryptlib.h"
+using std namespace std;
+
+
+
+
